413de0a7e58a162756da14559655c080d004d7e1
ndthang <thang.nguyen@yoonng.vn>
2024-04-05 17:57:31 +0700

    update

--- contracts/nft.sol
+++ contracts/nft.sol
@@ -2,105 +2,312 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract DNFT is ERC721URIStorage, Ownable {
-    uint256 private _tokenIds;
-    // Base URI required to interact with IPFS
-    string private _baseURIExtended;
+error NFTMarket__Price(string message);
+error NFTMarket__ListingFee(uint256 requiered, string message);
+error NFTMarket__SetListingFee(string message);
+error NFTMarket__ItemId(string message);
 
-    // Mapping to track the rewards received by each user
-    mapping(address => uint256) private rewardsReceived;
-    // Mapping to track the NFTs minted by each user
-    mapping(address => uint256[]) private userMintedTokens;
-    // Mapping to check if a token has already been minted
-    mapping(string => bool) private tokenExists;
+contract NFT is  ReentrancyGuard {
+    event marketItemNFT(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        uint256 createAt,
+        bool sold
+    );
 
-    constructor() ERC721("DNFT", "DNFT") Ownable(msg.sender) {
-        _setBaseURI("ipfs://");
+    uint256 private s_itemIds;
+    uint256 private s_itemsSold;
+
+    address payable private s_owner;
+    uint256 private s_listingFee;
+
+    struct MarketItem {
+        uint256 itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        uint256 createAt;
+        bool sold;
     }
 
-    // Sets the base URI for the collection
-    function _setBaseURI(string memory baseURI) private {
-        _baseURIExtended = baseURI;
+    mapping(uint256 => MarketItem) private s_MarketItems;
+
+    constructor() {
+        s_owner = payable(msg.sender);
+        s_listingFee = 0.0025 ether;
     }
 
-    // Overrides the default function to enable ERC721URIStorage to get the updated baseURI
-    function _baseURI() internal view override returns (string memory) {
-        return _baseURIExtended;
+    function createMarketItem(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        if (price <= 0)
+            revert NFTMarket__Price({message: "Price must be above zero"});
+        if (msg.value != s_listingFee)
+            revert NFTMarket__ListingFee({
+                requiered: s_listingFee,
+                message: "Price must be equal to listing price"
+            });
+
+        s_itemIds++;
+        uint256 itemId = s_itemIds;
+        uint256 createAt = block.timestamp;
+
+        s_MarketItems[itemId] = MarketItem({
+            itemId: itemId,
+            nftContract: nftContract,
+            tokenId: tokenId,
+            seller: payable(msg.sender),
+            owner: payable(address(0)),
+            price: price,
+            createAt: createAt,
+            sold: false
+        });
+
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        emit marketItemNFT(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price,
+            createAt,
+            false
+        );
     }
 
-    // Allows minting of a new NFT 
-    function mintCollectionNFT(address collector, string memory metadataURI) public onlyOwner() {
-        uint256 tokenId = ++_tokenIds;
-        _safeMint(collector, tokenId);
-        _setTokenURI(tokenId, metadataURI);
-
-    }
-
-    function mintNFT(address recipient, string memory tokenURI)
+    function buyNFT(address nftContract, uint256 itemId)
         public
+        payable
+        nonReentrant
+    {
+        uint256 price = s_MarketItems[itemId].price;
+        uint256 tokenId = s_MarketItems[itemId].tokenId;
+        if (msg.value != price)
+            revert NFTMarket__Price({
+                message: "Please submit the asking price in order to complete purchase"
+            });
+        s_MarketItems[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        s_MarketItems[itemId].owner = payable(msg.sender);
+        s_MarketItems[itemId].sold = true;
+        s_itemsSold++;
+        payable(s_owner).transfer(s_listingFee);
+        emit marketItemNFT(
+            itemId,
+            nftContract,
+            tokenId,
+            s_MarketItems[itemId].seller,
+            msg.sender,
+            price,
+            s_MarketItems[itemId].createAt,
+            true
+        );
+    }
+
+    function getMarketItems() public view returns (MarketItem[] memory) {
+        uint256 itemsCount = s_itemIds;
+        uint256 unsoldItemsCount = itemsCount - s_itemsSold;
+        MarketItem[] memory items = new MarketItem[](unsoldItemsCount);
+        uint256 currentIndex = 0;
+        for (uint256 i = 0; i < itemsCount; i++) {
+            if (!s_MarketItems[i + 1].sold) {
+                uint256 currentItemId = s_MarketItems[i + 1].itemId;
+                MarketItem storage marketItem = s_MarketItems[currentItemId];
+                items[currentIndex] = marketItem;
+                currentIndex++;
+            }
+        }
+
+        return items;
+    }
+
+    function getNFTByOwner() public view returns (MarketItem[] memory) {
+        uint256 totalItemsCount = s_itemIds;
+        uint256 itemCount = getItemOwnerCount(totalItemsCount);
+        uint256 currentIndex = 0;
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint256 i = 0; i < totalItemsCount; i++) {
+            if (s_MarketItems[i + 1].owner == msg.sender) {
+                uint256 currentId = s_MarketItems[i + 1].itemId;
+                MarketItem storage currentItem = s_MarketItems[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex++;
+            }
+        }
+        return items;
+    }
+
+    function getNFTBySeller() public view returns (MarketItem[] memory) {
+        uint256 totalItemsCount = s_itemIds;
+        uint256 itemCount = getItemSellerCount(totalItemsCount);
+        uint256 currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint256 i = 0; i < totalItemsCount; i++) {
+            if (s_MarketItems[i + 1].seller == msg.sender) {
+                uint256 currentId = s_MarketItems[i + 1].itemId;
+                MarketItem storage currentItem = s_MarketItems[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex++;
+            }
+        }
+        return items;
+    }
+
+    function getItemOwnerCount(uint256 totalItemsCount)
+        private
+        view
         returns (uint256)
     {
-        require(!tokenExists[tokenURI], "Token already exists");
-
-        uint256 newItemId = ++_tokenIds;
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        // Reward the user with 1 ETH.
-        rewardUser(recipient);
-
-        // Track the NFTs minted by the user
-        userMintedTokens[recipient].push(newItemId);
-
-        // Mark the token as minted
-        tokenExists[tokenURI] = true;
-
-        return newItemId;
+        uint256 itemCount = 0;
+        for (uint256 i = 0; i < totalItemsCount; i++) {
+            if (s_MarketItems[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+        return itemCount;
     }
 
-    function rewardUser(address user) internal {
-        uint256 rewardAmount = 100 wei;
-
-        require(address(this).balance >= rewardAmount, "Not enough Ether to reward user");
-
-        // Update the rewardsReceived mapping
-        rewardsReceived[user] += rewardAmount;
-
-        // Transfer the reward to the user.
-        (bool success, ) = payable(user).call{value: rewardAmount}("");
-        require(success, "Failed to send wei");
-
-        emit UserRewarded(user, rewardAmount);
+    function getItemSellerCount(uint256 totalItemsCount)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 itemCount = 0;
+        for (uint256 i = 0; i < totalItemsCount; i++) {
+            if (s_MarketItems[i + 1].seller == msg.sender) {
+                itemCount += 1;
+            }
+        }
+        return itemCount;
     }
 
-    // This function allows the contract owner to deposit ETH into the contract
-    function depositETH() public payable {}
-
-    // This function will allow the owner to withdraw all the remaining ETH.
-    function withdrawETH() public onlyOwner {
-        uint balance = address(this).balance;
-        payable(owner()).transfer(balance);
+    function getOwner() public view returns (address) {
+        return s_owner;
     }
 
-    // Function to get the total amount of rewards an user received
-    function totalRewards(address user) public view returns (uint256) {
-        return rewardsReceived[user];
+    function getListingFee() public view returns (uint256) {
+        return s_listingFee;
     }
 
-    // Function to get all tokens minted by a user
-    function getMintedTokens(address user) public view returns (uint256[] memory) {
-        return userMintedTokens[user];
+    function setListingFee(uint256 _price) public {
+        if (msg.sender != s_owner)
+            revert NFTMarket__SetListingFee({message: "Premission denied"});
+        s_listingFee = _price;
     }
 
-    // Function to check if a token with a given URI already exists
-    function checkTokenExists(string memory tokenURI) public view returns (bool) {
-        return tokenExists[tokenURI];
+    function getItemById(uint256 _id) public view returns (MarketItem memory) {
+        if (_id < 1 && _id > s_itemIds)
+            revert NFTMarket__ItemId({message: "Premission denied"});
+        return s_MarketItems[_id];
     }
 
-    // Event emitted when a user is rewarded
-    event UserRewarded(address indexed user, uint256 rewardAmount);
+    function getTotalItems() public view returns (uint256 total) {
+        uint256 itemsCount = s_itemIds;
+        uint256 unsoldItemsCount = itemsCount - s_itemsSold;
+        return unsoldItemsCount;
+    }
 
+    function fetchMarketItems(
+        uint256 offset,
+        uint256 limit,
+        uint256 sold
+    )
+        public
+        view
+        returns (
+            MarketItem[] memory,
+            uint256 nextOffset,
+            uint256 totalSolded
+        )
+    {
+        uint256 itemsCount = s_itemIds;
+        uint256 unsoldItemsCount = itemsCount - s_itemsSold;
+
+        if (limit == 0) {
+            limit = 1;
+        }
+
+        if (limit > unsoldItemsCount - offset) {
+            limit = unsoldItemsCount - offset;
+        }
+
+        MarketItem[] memory items = new MarketItem[](limit);
+
+        uint256 currentIndex = 0;
+        uint256 index = 0;
+        uint256 solded = 0;
+
+        for (uint256 i = 0; i < itemsCount && currentIndex < limit; i++) {
+            index = offset + i + sold + 1;
+            if (index <= itemsCount) {
+                if (!s_MarketItems[index].sold) {
+                    uint256 currentItemId = s_MarketItems[index].itemId;
+                    MarketItem storage marketItem = s_MarketItems[
+                        currentItemId
+                    ];
+                    items[currentIndex] = marketItem;
+                    currentIndex++;
+                } else {
+                    solded++;
+                }
+            }
+        }
+
+        if ((offset + sold + 1) > itemsCount) {
+            solded = sold;
+        }
+
+        return (items, offset + limit, solded);
+    }
+
+    function fetchMarketItemsByTime(uint256 time, uint256 limit)
+        public
+        view
+        returns (MarketItem[] memory)
+    {
+        uint256 itemsCount = s_itemIds;
+        uint256 unsoldItemsCount = itemsCount - s_itemsSold;
+        uint256 offset = unsoldItemsCount - 2;
+        if (limit == 0) {
+            limit = 1;
+        }
+
+        if (limit > unsoldItemsCount - offset) {
+            limit = unsoldItemsCount - offset;
+        }
+
+        MarketItem[] memory items = new MarketItem[](limit);
+
+        uint256 currentIndex = 0;
+        for (uint256 i = 0; i < itemsCount && currentIndex < limit; i++) {
+            if (!s_MarketItems[offset + i + 1].sold) {
+                uint256 currentItemId = s_MarketItems[offset + i + 1].itemId;
+                MarketItem storage marketItem = s_MarketItems[currentItemId];
+                if (
+                    time == 0 ||
+                    (marketItem.createAt <= block.timestamp &&
+                        marketItem.createAt >= time)
+                ) {
+                    items[currentIndex] = marketItem;
+                    currentIndex++;
+                }
+            }
+        }
+        return items;
+    }
 }
